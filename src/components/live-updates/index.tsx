e386fe1250aa6153df668d26b27bffa649c5e038
ciekotics <rahul.saxena@jabotics.in>
2024-07-08 16:03:47 +0530

    add to favorite implement

--- src/components/live-updates/index.tsx
+++ src/components/live-updates/index.tsx
@@ -2,7 +2,7 @@
 const LiveUpdates = () => {
   return (
     <div className="w-5/6 h-5/6 overflow-hidden rounded-2xl">
-      <div></div>
+      <div className="h-40 bg-black"> hey</div>
       <div></div>
     </div>
   )
