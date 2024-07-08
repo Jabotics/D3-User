e54c051f04504bf719d4fd76fd0c66dcc409048a
ciekotics <rahul.saxena@jabotics.in>
2024-07-08 15:29:08 +0530

    responsive pay play ui

--- src/components/pay&play/venues/VenueItem.tsx
+++ src/components/pay&play/venues/VenueItem.tsx
@@ -28,12 +28,12 @@ const VenueItem = ({ item }: { item: IGround }) => {
   const navigate = useNavigate();
 
   const openDetailsPage = (id: string) => {
-    navigate(`/details/?id=${id}`);
+    navigate(`/details?id=${id}`);
   };
 
   return (
-    <div className="flex rounded-md overflow-hidden w-full h-32 border border-gray-300 ">
-      <div className="w-60 ">
+    <div className="flex rounded-md overflow-hidden w-[90vw] sm:w-full h-24 sm:h-32 border border-gray-100 sm:border-gray-300 ">
+      <div className="w-24 sm:w-60">
         <img
           src={
             item?.images.length > 0
@@ -41,11 +41,11 @@ const VenueItem = ({ item }: { item: IGround }) => {
               : `${venueImg}`
           }
           alt="venueImg"
-          className="h-full w-full object-cover object-center rounded-l-md"
+          className="h-full w-full object-cover object-center rounded-md sm:rounded-l-md"
         />
       </div>
-      <div className="flex-1 flex flex-row gap-4 py-2 sm:py-2 px-4 sm:px-5">
-        <div className="flex flex-col w-full">
+      <div className="hidden flex-1 sm:flex flex-row gap-4 py-2 sm:py-2 px-4 sm:px-5">
+        <div className="w-full flex flex-col">
           <div className="flex flex-row w-full gap-2 justify-between items-center mt-3">
             <span className="inline-block w-[40%] text-[14px] sm:text-[16px] md:text-base font-bold">
               {item?.name}
@@ -112,6 +112,76 @@ const VenueItem = ({ item }: { item: IGround }) => {
               <span>Live</span>
             </Button>
           </div>
+        </div>
+      </div>
+
+      <div className="flex sm:hidden flex-1 flex-col justify-center gap-1 px-2">
+        <div className="h-4 w-full flex items-center justify-between ">
+          <span className="inline-block w-[40%] text-[14px] sm:text-[16px] md:text-base font-bold">
+            {item?.name}
+          </span>
+          <div className="flex items-end gap-2">
+            <Button
+              className="bg-[#252525] w-20 h-6 text-[8px] rounded-lg"
+              onClick={() => openDetailsPage(item?.id)}
+            >
+              View Details
+            </Button>
+            <div className="h-[22px] w-[22px] p-1  bg-[#54a63fb3] rounded-lg flex items-center justify-center ">
+              <IoIosHeartEmpty className="font-bold " size={20} color="white" />
+            </div>
+          </div>
+        </div>
+        <div className="w-full overflow-hidden h-4 flex items-start justify-between gap-2 mt-1">
+          <span className="flex-1 flex items-center flex-wrap">
+            {item?.supported_sports &&
+              item.supported_sports.map((item, index) => {
+                return (
+                  <p
+                    key={index}
+                    className="text-[8px] bg-gray-300 px-3 rounded-md"
+                  >
+                    {item.name}
+                  </p>
+                );
+              })}
+          </span>
+          <div className="flex items-center gap-1">
+            <FaLocationDot size={12} color="#D0D0D0" />
+            <p className="text-[8px] text-[#676767]">
+              {item?.venue?.name}, {item?.venue?.address}
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex items-start gap-3 h-4 mt-1">
+          <Button
+            variant={"outline"}
+            className="border-[#54a63f] hover:bg-[#70a862] text-xs h-full hover:text-white w-20 border rounded-sm "
+            onClick={() => {
+              dispatch(setSelectedGroundId(item.id));
+              navigate("/booking");
+            }}
+          >
+            Book Now
+          </Button>
+          <Button
+            variant={"outline"}
+            className="text-xs h-full w-20 border rounded-sm flex items-center justify-center gap-2"
+            onClick={() => {
+              dispatch(setSelectedGroundId(item.id));
+              navigate(
+                `/scoreboard?playid=${
+                  !!generateRandomString()
+                    ? `${generateRandomString(200)}-${item.id}`
+                    : ""
+                }`
+              );
+            }}
+          >
+            <span className="animate-fade-in-out bg-rose-700 h-2 w-2 rounded-full"></span>
+            <span>Live</span>
+          </Button>
         </div>
       </div>
     </div>
