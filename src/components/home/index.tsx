d10f2a674f6d1032c47248011225aec4e0827874
ciekotics <rahul.saxena@jabotics.in>
2024-08-12 15:42:58 +0530

    blogs redirect in homepage

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -9,6 +9,8 @@ import Events from "./event";
 import Faqs from "./faq";
 import Chat from "./chat";
 import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 // import D3App from "./d3-app";
 
 export const HomePage = () => {
@@ -36,6 +38,30 @@ export const HomePage = () => {
       <div className="w-[100vw] h-[65vh] overflow-hidden">
         <SliderSection />
       </div>
+
+      <section className="block px-5 py-3 lg:hidden w-full h-40">
+        <div className="w-full h-full bg-black rounded-xl overflow-hidden flex items-center justify-start relative">
+          <img
+            src="/images/hockey_olympics.webp"
+            alt=""
+            className="w-full h-full object-cover object-top z-0"
+          />
+          <div className="absolute pl-5">
+            <h1 className="text-white text-2xl font-bold">D3 Blogs</h1>
+            <p className="text-xs text-white">
+              Read all out blogs and stay updated about various sports events
+              across nation.
+            </p>
+
+            <Link to={"/blogs"}>
+              <Button className="mt-5 h-8 px-10 bg-white text-black">
+                Blogs
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+
       <Category />
       <HowitWorks />
       <Slots />
