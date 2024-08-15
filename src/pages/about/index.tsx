e80f36560776906a478df797339f47c247c6eb1c
nishantmishra9504 <nishant.mishra@jabotics.in>
2024-08-15 10:53:02 +0530

    testing deployment

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -12,8 +12,7 @@ import { useNavigate } from "react-router-dom";
 import { FaFacebook } from "react-icons/fa";
 import { LuInstagram } from "react-icons/lu";
 
-const AboutPage = () => {
-  console.log('testing');
+const AboutPage = () => {  
   const navigate = useNavigate();
 
   const controls1 = useAnimation();
