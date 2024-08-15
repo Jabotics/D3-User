1c4fd1d8aebda486ac7ba2e3815f5296f39fd1f0
ciekotics <rahul.saxena@jabotics.in>
2024-08-15 15:22:46 +0530

    changes

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,16 +1,19 @@
 import EventSection from "@/components/event/EventSection";
+import { useEffect } from "react";
 // import FilterSection from "@/components/event/FilterSection";
 
 const Event = () => {
-    return (
-        <>
-            <div className="flex flex-row gap-2 min:h-[90vh] w-[100%] self-center justify-center p-4">
-                {/* <FilterSection /> */}
-                <EventSection />
-            </div>
-
-        </>
-    )
-}
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <div className="flex flex-row gap-2 min-h-[90vh] max-h-fit w-[100%] self-center justify-center p-4">
+        {/* <FilterSection /> */}
+        <EventSection />
+      </div>
+    </>
+  );
+};
 
 export default Event;
